4407667d0753193fe480126b0a9525d9325cf569
KieranHsieh <kieran.hsieh@gmail.com>
2021-07-26 12:52:25 -0500

    Hello world in an IDE and text editor

---
+++ code_along_ide_hello_world/src/com/mthree/HelloWorld/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * @author Kieran Hsieh
+ * email: kieran.hsieh@gmail.com
+ * date: 7/26/2021
+ * purpose: My first Hello World program in an IDE
+ */
+
+package com.mthree.HelloWorld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
