5ab791b0d44718c873b4c93e8c1480c61909d7f3
KieranHsieh <kieran.hsieh@gmail.com>
2021-07-26 12:53:58 -0500

    Change Hello.java to greet myself

--- code_along_ide_hello_world/src/com/mthree/HelloWorld/Hello.java
+++ code_along_ide_hello_world/src/com/mthree/HelloWorld/Hello.java
@@ -9,6 +9,6 @@ package com.mthree.HelloWorld;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Kieran!");
     }
 }
